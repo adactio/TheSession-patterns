ae48430d72af29537414663b6dd668fda3c50626
Jeremy Keith <jeremy@adactio.com>
2016-02-16 11:17:28 +0000

    Set up preview template

--- fractal.js
+++ fractal.js
@@ -12,3 +12,13 @@ fractal.set('components.path', 'src/components');
  * Tell Fractal where to look for documentation pages.
  */
 fractal.set('pages.path', 'src/pages');
+
+/*
+ * Tell Fractal which file to use for rendering previews of patterns.
+ */
+fractal.set('components.preview.layout', '@preview');
+
+/*
+ * Tell Fractal where to render the generated output.
+ */
+fractal.set('plugins.web.static.path', 'public');
