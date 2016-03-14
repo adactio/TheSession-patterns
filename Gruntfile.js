b3ecc1edf17d3701c39445e53569dd9f61d1e09c
Jeremy Keith <jeremy@adactio.com>
2016-03-14 11:41:21 +0000

    Update grunt tasks to include JS concatenation, linting, and minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,20 +27,47 @@ module.exports = function(grunt) {
                 }
             }
         },
+        concat: {
+            dist: {
+                src: ['assets/js/*.js', 'src/components/**/*.js'],
+                dest: 'public_html/js/global.js'
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true
+            },
+            my_target: {
+                files: {
+                    'public/js/global-min.js': 'public/js/global.js'
+                }
+            }
+        },
+        jshint: {
+            beforeconcat: ['assets/js/*.js', 'src/components/**/*.js'],
+            afterconcat: ['public/js/global.js']
+        },
         watch: {
             css: {
                 files: ['assets/scss/**/*.scss', 'src/components/**/*.scss'],
                 tasks: ['sass_globbing', 'sass'],
                 options: {}
+            },
+            scripts: {
+                files: ['assets/js/**/*.js', 'src/components/**/*.js'],
+                tasks: ['jshint:beforeconcat', 'concat', 'jshint:afterconcat', 'uglify']
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-sass-globbing');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['sass_globbing', 'sass']);
+    grunt.registerTask('default', ['sass_globbing', 'sass', 'jshint:beforeconcat', 'concat', 'jshint:afterconcat', 'uglify']);
 
 };
