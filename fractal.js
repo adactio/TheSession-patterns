d899d488e1f9d2e7137f6864b65525ac400a2be3
Jeremy Keith <jeremy@adactio.com>
2016-08-28 13:43:00 +0100

    Update Fractal settings to version 1

--- fractal.js
+++ fractal.js
@@ -1,29 +1,20 @@
-/*
- * Require the Fractal module
- */
-var fractal = require('@frctl/fractal');
+/* Create a new Fractal instance and export it for use elsewhere if required */
+const fractal = module.exports = require('@frctl/fractal').create();
 
-/*
- * Add a project title
- */
+/* Set the title of the project */
 fractal.set('project.title', 'Patterns from The Session');
 
-/*
- * Tell Fractal where to look for components.
- */
-fractal.set('components.path', 'src/components');
+/* Tell Fractal where the components will live */
+fractal.components.set('path', __dirname + '/src/components');
 
-/*
- * Tell Fractal where to look for documentation pages.
- */
-fractal.set('docs.path', 'src/docs');
+/* Tell Fractal where the documentation pages will live */
+fractal.docs.set('path', __dirname + '/src/docs');
 
-/*
- * Tell Fractal which file to use for rendering previews of patterns.
- */
-fractal.set('components.preview.layout', '@preview');
+/* Tell Fractal which file to use for rendering previews of patterns */
+fractal.components.set('default.preview', '@preview');
 
-/*
- * Tell Fractal where to render the generated output.
- */
-fractal.set('plugins.web.static.path', 'public');
+/* Specify a directory of static assets */
+fractal.web.set('static.path', __dirname + '/public');
+
+/* Set the static HTML build destination */
+fractal.web.set('builder.dest', __dirname + '/build');
