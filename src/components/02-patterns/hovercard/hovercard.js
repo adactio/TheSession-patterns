f949780ebf417374264c990be6695b3a95c48991
Jeremy Keith <jeremy@adactio.com>
2016-08-28 16:36:36 +0100

    Add hovercard pattern

--- src/components/02-patterns/hovercard/hovercard.js
+++ src/components/02-patterns/hovercard/hovercard.js
@@ -123,38 +123,3 @@ Show mini member profiles on hover after a delay
     init();
 
 }(this, this.document));
-
-/*
-Show and hide content with aria-controls buttons
-*/
-(function (win, doc) {
-    'use strict';
-    if (!win.addEventListener) {
-        // doesn't cut the mustard.
-        return;
-    }
-    var toggleID;
-    var togglecontent;
-    var target;
-    function toggle(ev) {
-        ev = ev || win.event;
-        target = ev.target;
-        if (target.hasAttribute('aria-controls')) {
-            toggleID = target.getAttribute('aria-controls');
-            if (doc.getElementById(toggleID)) {
-                ev.preventDefault();
-                togglecontent = doc.getElementById(toggleID);
-                if (togglecontent.getAttribute('aria-hidden') == 'true') {
-                    togglecontent.setAttribute('aria-hidden', 'false');
-                    togglecontent.setAttribute('tabindex', '-1');
-                    togglecontent.focus();
-                    target.setAttribute('aria-expanded', 'true');
-                } else {
-                    togglecontent.setAttribute('aria-hidden', 'true');
-                    target.setAttribute('aria-expanded', 'false');
-                }
-            }
-        }
-    }
-    doc.addEventListener('click', toggle, false);
-}(this, this.document));
