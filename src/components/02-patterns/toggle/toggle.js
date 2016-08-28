86720e1f1b7f710dd5098b736b17e02b2b8a455c
Jeremy Keith <jeremy@adactio.com>
2016-08-28 16:25:56 +0100

    Adjust toggle pattern

--- src/components/02-patterns/toggle/toggle.js
+++ src/components/02-patterns/toggle/toggle.js
@@ -3,36 +3,26 @@ Show and hide content with aria-controls buttons
 */
 (function (win, doc) {
     'use strict';
-    if (!doc.querySelectorAll || !win.addEventListener) {
+    if (!win.addEventListener) {
         // doesn't cut the mustard.
         return;
     }
-    var toggles = doc.querySelectorAll('.toggle button[aria-controls]');
-    var togglecount = toggles.length;
     var toggleID;
     var togglecontent;
-    var i;
     var target;
-    for (i = 0; i < togglecount; i = i + 1) {
-        toggleID = toggles[i].getAttribute('aria-controls');
-        if (doc.getElementById(toggleID)) {
-            togglecontent = doc.getElementById(toggleID);
-            togglecontent.setAttribute('aria-hidden', 'true');
-            togglecontent.setAttribute('tabindex', '-1');
-            toggles[i].setAttribute('aria-expanded', 'false');
-        }
-    }
     function toggle(ev) {
         ev = ev || win.event;
-        target = ev.target || ev.srcElement;
+        target = ev.target;
         if (target.hasAttribute('aria-controls')) {
             toggleID = target.getAttribute('aria-controls');
             if (doc.getElementById(toggleID)) {
+                ev.preventDefault();
                 togglecontent = doc.getElementById(toggleID);
                 if (togglecontent.getAttribute('aria-hidden') == 'true') {
                     togglecontent.setAttribute('aria-hidden', 'false');
-                    target.setAttribute('aria-expanded', 'true');
+                    togglecontent.setAttribute('tabindex', '-1');
                     togglecontent.focus();
+                    target.setAttribute('aria-expanded', 'true');
                 } else {
                     togglecontent.setAttribute('aria-hidden', 'true');
                     target.setAttribute('aria-expanded', 'false');
